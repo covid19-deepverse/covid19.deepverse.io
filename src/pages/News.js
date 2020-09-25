73cd1ea1b10ae94e800365050c5fa95caab9e0e5
oat phattaraphon <phattaraphon.c@ku.th>
2020-09-26 02:28:05 +0700

    adding news page

---
+++ src/pages/News.js
@@ -0,0 +1,16 @@
+/**
+ *   News Page
+ *
+ */
+
+import React from 'react';
+
+function News() {
+  return (
+    <div className="news">
+      <h1>News</h1>
+    </div>
+  );
+}
+
+export default News;
