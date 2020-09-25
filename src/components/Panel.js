9bdf0bd2028653ba82f07398bc0b0e77683269b3
oat phattaraphon <phattaraphon.c@ku.th>
2020-09-26 06:48:04 +0700

    adding panel component

---
+++ src/components/Panel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/sass/components/_panel.scss';
+
+function Panel() {
+  return (
+    <div className="panel">
+      <h1>Hello Panel</h1>
+    </div>
+  );
+}
+
+export default Panel;
