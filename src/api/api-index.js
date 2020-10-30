bd37f5500869a3a299fda08683bf106569a226e7
Nattapon Phumutta <47663338+phumutta@users.noreply.github.com>
2020-10-30 22:02:49 +0700

    add get country

--- src/api/api-index.js
+++ src/api/api-index.js
@@ -7,3 +7,16 @@ const url="https://covid19.mathdro.id/api"
 app.get('/', (req, res) => {
   res.send('Hello World : API SERVER')
 })
+app.get('/getCountry', (req, res) => {
+   
+    let changeableUrl=url;
+  
+      request(changeableUrl,
+        function(error,response,body){
+            if(!error && response.statusCode ==200){
+                var parsedBody=JSON.parse(body)
+                res.send(parsedBody)
+            }
+        })
+   
+  })
